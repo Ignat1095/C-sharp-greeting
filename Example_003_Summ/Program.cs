02e59b4b81b9994cc8b221adc2f7a10f0fe9e87b
Ignat <Ignat1095@mail.ru>
2022-08-03 22:02:35 +0300

    add random

---
+++ Example_003_Summ/Program.cs
@@ -0,0 +1,8 @@
+﻿int num_a = new Random().Next(1,10);
+Console.WriteLine(num_a);
+
+int num_b = new Random().Next(1,10);
+Console.WriteLine(num_b);
+
+int result = num_a + num_b;
+Console.WriteLine(result);
